2b3e561bb1f549efb89ff0f4e96ef68b7345ab3c
ambusch <46773401+ambusch@users.noreply.github.com>
2019-04-09 02:54:17 -0500

    Create fortune1000iowa.js

---
+++ Assign3/fortune1000iowa.js
@@ -0,0 +1,113 @@
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#008000",
+        "marker-size": "medium",
+        "marker-symbol": "commercial",
+        "Name": "Principal Financial Group, Inc.",
+        "Rank": 210,
+        "Years on Fortune 500": 24
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -93.6275465,
+          41.5886935
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#008000",
+        "marker-size": "medium",
+        "marker-symbol": "commercial",
+        "Name": "Rockwell Collins, Inc.",
+        "Rank": 415,
+        "Years on Fortune 500": 7
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -91.6394176,
+          42.0319988
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#008000",
+        "marker-size": "medium",
+        "marker-symbol": "commercial",
+        "Name": "Casey's General Stores, Inc.",
+        "Rank": 425,
+        "Years on Fortune 500": 8
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -93.57317,
+          41.693711
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#008000",
+        "marker-size": "medium",
+        "marker-symbol": "commercial",
+        "Name": "American Equity Investment Life Holding Company",
+        "Rank": 614,
+        "Years on Fortune 500": "n/a"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -93.7916559,
+          41.5944859
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#008000",
+        "marker-size": "medium",
+        "marker-symbol": "commercial",
+        "Name": "HNI Corporation",
+        "Rank": 913,
+        "Years on Fortune 500": "n/a"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -91.0395377,
+          41.4240295
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#008000",
+        "marker-size": "medium",
+        "marker-symbol": "commercial",
+        "Name": "Renewable Energy Group, Inc.",
+        "Rank": 918,
+        "Years on Fortune 500": "n/a"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -93.5733309,
+          42.0185525
+        ]
+      }
+    }
+  ]
+}
